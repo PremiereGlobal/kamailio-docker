6d5eb03e7bdb9dd7fb96514d76ea5762e63da7ca
Luke Wahlmeier <luke.wahlmeier@readytalk.com>
2018-05-22 11:29:33 -0600

    updated build process

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+docker build -t kamailio-docker:latest .
+
+KAM_TMP=$(docker run --rm --entrypoint="" -it kamailio-docker:latest /usr/sbin/kamailio -V | tr " " "\n")
+X=($(echo $KAM_TMP | tr " " "\n"))
+
+KAM_VER=${X[2]}
+
+KAM_A=(${KAM_VER//./ })
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+docker tag rt-freeswitch:latest readytalk/kamailio-docker:${KAM_VER}
+docker tag rt-freeswitch:latest readytalk/kamailio-docker:${KAM_A[0]}.${KAM_A[1]}
+docker tag rt-freeswitch:latest readytalk/kamailio-docker:latest
+echo "-----------------------"
+echo "Saved Tag \"kamailio-docker:${KAM_VER}\""
+echo "Saved Tag \"kamailio-docker:${KAM_A[0]}.${KAM_A[1]}\""
+echo "Saved Tag \"kamailio-docker:latest\""
+echo "-----------------------"
+
+if [[ ${TRAVIS} && "${GIT_BRANCH}" == "master" ]]; then
+  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  docker push readytalk/kamailio-docker:${KAM_VER}
+  docker push readytalk/kamailio-docker:${KAM_A[0]}.${KAM_A[1]}
+  docker push readytalk/kamailio-docker:latest
+fi
--- docker_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker rm -f mysql-kamailio
-docker rm -f kamailio
-
-docker run -v ${PWD}/mysql_sock:/var/run/mysqld -v ${PWD}/ssl:/etc/kamailio/ssl --network host --name kamailio -p 443:443/tcp -p 5060:5060/udp -p 5060:5060/tcp -p 5061:5061/tcp -itd readytalk/kamailio-docker:latest
