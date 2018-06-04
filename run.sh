769c97ef660d8d362c0e4aeeb59cf481b7afd341
Luke Wahlmeier <luke.wahlmeier@readytalk.com>
2018-06-04 10:35:22 -0600

    updated configs/run to use more/better environment vars

--- run.sh
+++ run.sh
@@ -2,6 +2,25 @@
 
 . /env.sh
 
+DEFAULT_KAM_DEFINE_DB_MOD_FILE="db_sqlite.so"
+DEFAULT_KAM_DEFINE_DBURL="sqlite:////etc/kamailio/kamailio.sqlite"
+DEFAULT_KAM_DEFINE_WITH_UDP="true"
+DEFAULT_KAM_DEFINE_WITH_TCP="true"
+DEFAULT_KAM_DEFINE_WITH_TLS="true"
+DEFAULT_KAM_DEFINE_WITH_WEBSOCKETS="true"
+DEFAULT_KAM_DEFINE_WITH_USRLOCDB="true"
+DEFAULT_KAM_DEFINE_WITH_ANTIFLOOD="true"
+DEFAULT_KAM_DEFINE_WITH_LOADBALANCE="true"
+DEFAULT_KAM_DEFINE_WITH_DEBUG="false"
+
+DEFAULT_KAM_DEFINE_TLS_CONFIG_PATH="/etc/kamailio/tls_mod.cfg"
+DEFAULT_KAM_INT_IP="0.0.0.0"
+DEFAULT_KAM_EXT_IP="127.0.0.1"
+DEFAULT_KAM_UDP_PORT="5060"
+DEFAULT_KAM_TCP_PORT="5060"
+DEFAULT_KAM_TLS_PORT="4443"
+
+
 for var in ${!DEFAULT_KAM*}; do
   t=${var/DEFAULT_/}
   if [ -z ${!t} ]; then
@@ -15,7 +34,11 @@ done
 
 for var in ${!KAM_*}; do
   if [[ $var == KAM_*  && "${var}" != "KAM_DISPATCHER_ROUTES" ]]; then
-    echo "#!substdef \"!${var}!${!var}!g\"" >> "/etc/kamailio/vars.cfg"
+    if [[ $var == KAM_DEFINE_* && ${!var} != "false" ]]; then
+      echo "#!define ${var/KAM_DEFINE_/} \"${!var}\"" >> "/etc/kamailio/defines.cfg"
+    else
+      echo "#!substdef \"!${var}!${!var}!g\"" >> "/etc/kamailio/vars.cfg"
+    fi
   fi
 done
 
@@ -25,6 +48,6 @@ if [[ -n ${KAM_DISPATCHER_ROUTES} ]]; then
   done
 fi
 
-
+openssl req -x509 -newkey rsa:4096 -keyout /etc/kamailio/kamailio-selfsigned.key -out /etc/kamailio/kamailio-selfsigned.pem -days 36500 -subj '/CN=localhost' -sha256 -nodes
 
 exec "${@}"
